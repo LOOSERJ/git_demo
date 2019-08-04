a6aa6eaa5b252856761f049fefdabdb3245878c8
dong <1309001881@qq.com>
2019-08-04 20:51:29 +0800

    第一次提交到git

---
+++ gitdemo/src/cong/domg/frist/Demo1.java
@@ -0,0 +1,14 @@
+package cong.domg.frist;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+    }
+
+}
